312328e4a6d0ce2b0b54a50f2a276ee007186719
Simon <shwaldemar@gmail.com>
2019-08-07 16:17:48 +0100

    can find whisky by year function in whisky repository is succesfully used by test

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,42 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void findWhiskysForAParticularYear(){
+		List<Whisky> found = whiskyRepository.findWhiskyByYear(1991);
+		assertEquals("The Rosebank 12 - Flora and Fona", found.get(0).getName());
+	}
+
+//	@Test
+//	public void findShipsThatHavePiratesNamed(){
+//		List<Ship> found  = shipRepository.findShipsThatHavePiratesNamed("John");
+//		assertEquals("The Flying Dutchman", found.get(0).getName());
+//	}
 
 }
